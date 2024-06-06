0540369fb852e6fa0c4cc1d43abcebdc2bac1259
maeagripa <157185016+maeagripa@users.noreply.github.com>
2024-06-06 08:44:51 -0700

    Update style.js

--- js/style.js
+++ js/style.js
@@ -1,40 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Responsive Navbar</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
+const lightbox = document.createElement('div')
+lightbox.id = 'lightbox'
+document.body.appendChild(lightbox)
 
-    <nav class="navbar">
-        <div class="project-title">Project <span class="project-title1">Title</span></div>
-        <div class="menu-toggle" id="mobile-menu">
-            <div></div>
-            <div></div>
-            <div></div>
-        </div>
-        <div class="navbar-links" id="navbar-links">
-            <ul>
-                <li><a href="#section1">Home</a></li>
-                <li><a href="#section2">About</a></li>
-                <li><a href="#skills">Skills</a></li>
-                <li><a href="#section3">Education</a></li>
-                <li><a href="#section4">Contact</a></li>
-            </ul>
-        </div>
-    </nav>
+const images = document.querySelectorAll('img')
+images.forEach(image => {
+  image.addEventListener('click', e => {
+    lightbox.classList.add('active')
+    const img = document.createElement('img')
+    img.src = image.src
+    while (lightbox.firstChild) { 
+      lightbox.removeChild(lightbox.firstChild)
+    }
+    lightbox.appendChild(img)
+  })
+})
 
-    <!-- Add your sections here -->
+lightbox.addEventListener('click', e => {
+  if (e.target !== e.currentTarget) return
+  lightbox.classList.remove('active')
+})
 
-    <script>
-        const mobileMenu = document.getElementById('mobile-menu');
-        const navbarLinks = document.getElementById('navbar-links');
+const menuToggle = document.querySelector('.menu-toggle');
+const navbarLinks = document.querySelector('.navbar-links');
 
-        mobileMenu.addEventListener('click', () => {
-            navbarLinks.classList.toggle('active');
-        });
-    </script>
-</body>
-</html>
+menuToggle.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active');
+});
